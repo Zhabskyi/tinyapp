a9aa934a42708d126b3c783acf3d9580106b02e9
Zhabskyi <poltavagaz@gmail.com>
2019-10-29 15:20:38 +0000

    Add delete request for short url and redirection to /urls

--- express_server.js
+++ express_server.js
@@ -49,6 +49,11 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${shortURL}`)
 });
 
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect('/urls');
+});
+
 app.get("/u/:shortURL", (req, res) => {
   console.log(req.params);
   const longURL = urlDatabase[req.params.shortURL] 
